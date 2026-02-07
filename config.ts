a8ac18033c9200dc037d0451ef05591fa1cc5a86
Islam <eng-islam@outlook.com>
2026-02-07 23:14:13 +0200

    "update_config"

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 // config.ts - إعدادات النظام الموحدة
 export const CONFIG = {
   // عنوان السيرفر (قم بتغييره عند الانتقال للـ VPS)
-  API_BASE_URL: 'http://157.245.169.126:3000',
+  API_BASE_URL: 'https://www.nebras-erp.com',
   
   // مفتاح الأمان للـ API (اختياري حالياً)
   API_KEY: 'authentic_secure_key_2025',
